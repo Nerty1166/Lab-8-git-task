c4de89979a1f300060090db7e12682264abed073
Nikita Gorbunov <nert138@gmail.com>
2024-10-05 08:46:48 +0300

    add Assert

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,6 @@
 ﻿begin
   var (year, flag) := (ReadInteger('Введите год:'), False);
+  Assert(year > 0);
   if (year.Divs(4) and not year.Divs(100)) or (year.Divs(400)) then
     flag := True;
   Print($'Год високосный?: {flag}');
