a7713f7e6749ee8b7745e28e3c93767b5c3f4c51
Nikita Gorbunov <nert138@gmail.com>
2024-10-05 08:42:14 +0300

    Add is leap year programm!

---
+++ DateTime.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var (year, flag) := (ReadInteger('Введите год:'), False);
+  if (year.Divs(4) and not year.Divs(100)) or (year.Divs(400)) then
+    flag := True;
+  Print($'Год високосный?: {flag}');
+end.
